571c5413af321022f644c57f95e398227bab0e3d
vladscriptgame <vladk55868@gmail.com>
2024-08-20 19:26:23 +0500

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,1 +1,48 @@
 print('GUI Активировано')
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+-- Создать окно UI
+local Window = Library.CreateLib("Name", "RJTheme3")
+
+-- Секция
+local Tab = Window:NewTab("TabName")
+
+-- Подсекция
+local Section = Tab:NewSection("Section Name")
+
+-- Заголовок
+Section:NewLabel("LabelText")
+
+-- Кнопка
+Section:NewButton("ButtonText", "ButtonInfo", function()
+    print("Clicked")
+end)
+
+-- Переключатель
+Section:NewToggle("ToggleText", "ToggleInfo", function(state)
+    if state then
+        print("Toggle On")
+    else
+        print("Toggle Off")
+    end
+end)
+
+-- Слайдер
+Section:NewSlider("SliderText", "SliderInfo", 500, 0, function(s) -- 500 (Макс. значение) | 0 (Мин. значение)
+    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = s
+end)
+
+-- Текст Бокс
+Section:NewTextBox("TextboxText", "TextboxInfo", function(txt)
+	print(txt)
+end)
+
+-- Бинд на клавиши
+Section:NewKeybind("KeybindText", "KeybindInfo", Enum.KeyCode.F, function()
+	print("You just clicked the bind")
+end)
+
+-- Дропдаун
+Section:NewDropdown("DropdownText", "DropdownInf", {"Option 1", "Option 2", "Option 3"}, function(currentOption)
+    print(currentOption)
+end)
+
